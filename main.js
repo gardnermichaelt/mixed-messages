df88112576d4bc054d9d0fba8a85a0c2e2008cf5
gardnermichaelt <92893082+gardnermichaelt@users.noreply.github.com>
2021-11-06 16:09:06 -0700

    begin building character factory

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const charFactory = (name, age, sex, gender, pronouns, job, jobSkill) => {
+    return {
+        name: name,//string
+        age: age,//number
+        sex: sex,//biological m or f
+        gender: gender,//how they think of themselves
+        pronouns: pronouns,//he, him his, she, her, hers
+        job: job,//string
+        jobSkill: jobSkill//skill level, number from 1 to 100. determines economic output
 
+    };//object container
+}//object factory container
